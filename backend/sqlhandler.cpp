889f1a75034f37a4ab1b856ba326a8660982d456
fdar0536 <fdar0536@users.noreply.github.com>
2025-11-11 03:01:49 +0800

    doc: add license

--- backend/sqlhandler.cpp
+++ backend/sqlhandler.cpp
@@ -1,3 +1,26 @@
+/*
+ * Font Color
+ * Copyright (c) 2019-present fdar0536
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #include <string>
 #include "sqlhandler.hpp"
 
@@ -199,7 +222,7 @@ void SQLHandler::db_init()
 
     std::string tmp_path(tmp_char);
     std::string::size_type str_index = 0;
-    while ((str_index = tmp_path.find("\\", str_index)) != string::npos)
+    while ((str_index = tmp_path.find("\\", str_index)) != std::string::npos)
     {
         tmp_path.replace(str_index, 1, "/");
         ++str_index;
